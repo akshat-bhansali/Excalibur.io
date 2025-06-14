f3e69976291d70f11eb109ae9a7b15683264e223
akshat-bhansali7 <akshat.bhansali@angelbroking.com>
2025-06-14 13:17:06 +0530

    trying to fix deployment error

--- src/App.jsx
+++ src/App.jsx
@@ -93,7 +93,7 @@ function App() {
         />
       )}
 
-      {!isGameStarted ? (
+      {/* {!isGameStarted ? (
         <PlayerProfileForm onSubmit={handleProfileSubmit} />
       ) : (
         <Canvas
@@ -119,7 +119,7 @@ function App() {
             </EffectComposer>
           )}
         </Canvas>
-      )}
+      )} */}
     </div>
   );
 }
