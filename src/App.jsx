5dad5a6d5a37eef34fa82b8bd9f179769263be24
akshat-bhansali7 <akshat.bhansali@angelbroking.com>
2025-06-14 13:43:54 +0530

    og

--- src/App.jsx
+++ src/App.jsx
@@ -93,7 +93,33 @@ function App() {
         />
       )}
 
-hi
+      {!isGameStarted ? (
+        <PlayerProfileForm onSubmit={handleProfileSubmit} />
+      ) : (
+        <Canvas
+          shadows
+          camera={{ position: [0, 30, 0], fov: 30, near: 2 }}
+          dpr={[1, 1.5]}
+        >
+          <color attach="background" args={["#242424"]} />
+          <SoftShadows size={42} />
+          <PerformanceMonitor onDecline={() => setDowngradedPerformance(true)} />
+          <Suspense>
+            <Physics>
+              <Experience
+                downgradedPerformance={downgradedPerformance}
+                playerData={playerData}
+                xpChange={xpChange}
+              />
+            </Physics>
+          </Suspense>
+          {!downgradedPerformance && (
+            <EffectComposer disableNormalPass>
+              <Bloom luminanceThreshold={1} intensity={1.5} mipmapBlur />
+            </EffectComposer>
+          )}
+        </Canvas>
+      )}
     </div>
   );
 }
