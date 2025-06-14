cec220c5892c32c3d2f2d96ca8891bc714aea1b2
akshat-bhansali7 <akshat.bhansali@angelbroking.com>
2025-06-14 13:39:52 +0530

    test

--- src/App.jsx
+++ src/App.jsx
@@ -94,32 +94,32 @@ function App() {
       )}
 
       {!isGameStarted ? (
-        <PlayerProfileForm onSubmit={handleProfileSubmit} />
-      ) : (
-        // <Canvas
-        //   shadows
-        //   camera={{ position: [0, 30, 0], fov: 30, near: 2 }}
-        //   dpr={[1, 1.5]}
-        // >
-        //   <color attach="background" args={["#242424"]} />
-        //   <SoftShadows size={42} />
-        //   <PerformanceMonitor onDecline={() => setDowngradedPerformance(true)} />
-        //   <Suspense>
-        //     <Physics>
-        //       <Experience
-        //         downgradedPerformance={downgradedPerformance}
-        //         playerData={playerData}
-        //         xpChange={xpChange}
-        //       />
-        //     </Physics>
-        //   </Suspense>
-        //   {!downgradedPerformance && (
-        //     <EffectComposer disableNormalPass>
-        //       <Bloom luminanceThreshold={1} intensity={1.5} mipmapBlur />
-        //     </EffectComposer>
-        //   )}
-        // </Canvas>
+        // <PlayerProfileForm onSubmit={handleProfileSubmit} />
         hi
+      ) : (
+        <Canvas
+          shadows
+          camera={{ position: [0, 30, 0], fov: 30, near: 2 }}
+          dpr={[1, 1.5]}
+        >
+          <color attach="background" args={["#242424"]} />
+          <SoftShadows size={42} />
+          <PerformanceMonitor onDecline={() => setDowngradedPerformance(true)} />
+          <Suspense>
+            <Physics>
+              <Experience
+                downgradedPerformance={downgradedPerformance}
+                playerData={playerData}
+                xpChange={xpChange}
+              />
+            </Physics>
+          </Suspense>
+          {!downgradedPerformance && (
+            <EffectComposer disableNormalPass>
+              <Bloom luminanceThreshold={1} intensity={1.5} mipmapBlur />
+            </EffectComposer>
+          )}
+        </Canvas>
       )}
     </div>
   );
